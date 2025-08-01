22159a2ee30666cf0fc7eb3903fd443c2b3c3c43
somya <somyasharma0488@gmail.com>
2025-08-01 16:30:29 +0530

    main+

---
+++ pip.py
@@ -0,0 +1,16 @@
+# List: A List is a collection which is ordered and unchangeable. It is written in square brackets[].
+
+list=["Apple", "banana", "mango"]
+print(list)
+
+#We can access list items by index number.
+print(list[2])
+
+#Negative Indexing: Indexing begins from the end but it starts form -1 and refers to the last element.
+print(list[-2]) 
+
+#List stores multiple items in a single variable and it is one of the 4 built-in data types in Python used to store collection of data.
+#List allow duplicate values.
+list=["eyes", "hair", "nose", "nail", "eyes"]
+print(list)
+
