1cfdbf91bf987b120cb08461f8b3bf0c1cd75cb5
somya <somyasharma0488@gmail.com>
2025-09-08 15:31:30 +0530

    main

---
+++ basic.py
@@ -0,0 +1,46 @@
+a="Hello"
+# print('h' not in a)
+# print('H' is not a)
+
+
+# x=1
+# y=2
+# print(bin(x))
+# print(bin(y))
+# print(x&y,bin(x&y))
+# print(x|y,bin(x|y))
+# print(x^y,bin(x^y))
+
+
+print(a, type(a))
+
+
+s='''Hello
+World'''
+print(s)
+
+
+# //list --> Mutable data type
+l=[1,2,3,4,'lol']   
+l[0]=4
+print(l)
+
+# //tuple --> Immutable data type
+t=(1,2,3,4,'lol')
+# t[3]='Jk'
+print(t)
+
+
+#dictionary
+d={
+    'name':'Somya',
+    'age':19,
+    'college':'VIT'
+}
+
+print(d['name'])
+
+
+# set--> Unordered collection of unique items
+s={1,2,3,4,5,5,5,5}
+print(s)
